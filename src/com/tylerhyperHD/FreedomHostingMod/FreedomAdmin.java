35545dc4707e6e971c89ee910533595dc45ac309
Tyler Wrenn <mcbeat257@gmail.com>
2015-11-16 17:54:39 -0500

    Add apache headers

--- src/com/tylerhyperHD/FreedomHostingMod/FreedomAdmin.java
+++ src/com/tylerhyperHD/FreedomHostingMod/FreedomAdmin.java
@@ -15,21 +15,10 @@
  */
 package com.tylerhyperHD.FreedomHostingMod;
 
-import java.util.logging.Logger;
-
-public class F_Log {
+/**
+ *
+ * @author tyler
+ */
+public class FreedomAdmin {
     
-    public static final Logger log = Logger.getLogger("Minecraft");
-    
-    public static void info(String args) {
-        log.info(args);
-    }
-    
-    public static void severe(String args) {
-        log.severe(args);
-    }
-
-    static void severe(Exception ex) {
-        log.severe(ex.toString());
-    }
 }
